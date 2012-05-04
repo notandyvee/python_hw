dc9f1b9651a99aaf606d2d6685b7918e88fb6d4f
Andy Valdez <avaldez@oswego.edu>
2012-05-04 17:08:13 -0400

    Made the lisp parser

--- cgi-bin/file_readers.py
+++ cgi-bin/file_readers.py
@@ -1,39 +1,41 @@
 import os
 import re
 #***********************START OF FUNCTION***********************************
-def clean_list(the_list, string):
+def clean_list(the_list):
 	#this function takes lists within a list and essentially relists it with just one main list
 	holder = []
 	for each_index in the_list:
 		if re.search('^[a-zA-Z]', each_index):
 			holder.append(each_index)
-			
+					
 	#check to see which list to use to strip away any uneccessary symbols		
-	if string == 'c':
-		da_list = ['char', 'double', 'float', 'int', 'long', 'short', 'struct', 'include', 'define', 'if', 'while', 'for', 'else', 'NULL', 'EOF', 'FILE']
-	elif string == 'lisp':
-		da_list = []
-	elif string == 'scala':
-		da_list = []
-	elif string == 'prolog':
-		da_list == []
-	elif string == 'python':
-		da_list == []
+#	if string == 'c':
+#		da_list = ['char', 'double', 'float', 'int', 'long', 'short', 'struct', 'include', 'define', 'if', 'while', 'for', 'else', 'NULL', 'EOF', 'FILE']
+#		do_this = True
+#	elif string == 'lisp':
+#		#da_list = ['defun', 'if']
+#		do_this = False
+#	elif string == 'scala':
+#		da_list = []
+#	elif string == 'prolog':
+#		da_list == []
+#	elif string == 'python':
+#		da_list == []
 		
-	holder2 = []
-	test = False
-	for each_index in holder:
-		for sym in da_list:
-			if each_index == sym:
-				test = True
-		if test == False:
-			holder2.append(each_index)
-		test = False	
+#	holder2 = []
+#	test = False
+#	for each_index in holder:
+#		for sym in da_list:
+#			if each_index == sym:
+#				test = True
+#		if test == False:
+#			holder2.append(each_index)
+#		test = False	
 	
 	#just removes duplicates
-	holder2 = set(holder2)
-				
-	return holder2	
+#	holder2 = set(holder2)
+	holder = set(holder)
+	return holder	
 	
 #***************************END OF FUNCTION**********************************
 
@@ -64,14 +66,16 @@ def parse_c_file():
 			elif ch != '':
 				list_holder.append(ch)
 				ch = ''
-	list_holder = clean_list(list_holder, 'c')
+	list_holder = clean_list(list_holder)
+	
 	#write the file symbols	
 	with open('symbols', "w") as file_to_write:
 		#add iteration, which should definitely be for loop
 		for each_index in list_holder:
 			print("[hw1.c, " + each_index + "]\n", file=file_to_write)		
 #*************************END OF FUNCTION*************************************	
-	
+
+#**************************START OF FUNCTION*********************************	
 def parse_lisp_file():
 	#make sure the current working directory is where the C file is
 	os.chdir('/Users/Zeroe/Documents/python_hw/csc344/a2')
@@ -81,14 +85,28 @@ def parse_lisp_file():
 		print("Failed to get to the page for some reason")
 	
 	#now you can open the file and start reading the c file	
+	ch = ''
+	list_holder = [];	
+	with open('hw2.lisp') as f:
+		while True:
+			c = f.read(1)
+			if not c:
+				print("End of File")
+				break
+			if re.search("\w", c):
+				ch = ch + c
+			elif ch != '':
+				list_holder.append(ch)
+				ch = ''
+	list_holder = clean_list(list_holder)
 	
+	#write the file symbols	
+	with open('symbols', "w") as file_to_write:
+		#add iteration, which should definitely be for loop
+		for each_index in list_holder:
+			print("[hw1.c, " + each_index + "]\n", file=file_to_write)
 	
-	
-	
-	
-	
-	
-	
+#******************************END OF FUNCTION*********************************	
 	
 def parse_scala_file():
 	#make sure the current working directory is where the C file is
--- csc344/a2/hw2.lisp
+++ csc344/a2/hw2.lisp
@@ -1,9 +1,3 @@
-;;;;Andy Valdez
-;;;;Homework 2
-;;;;3/19/2012
-
-
-
 (defun andexp (a b) 
 	(list 'and a b))
 	
@@ -13,11 +7,9 @@
 (defun notexp (a) 
 	(list 'not a))
 
-(setq p1 (andexp 'x (orexp 'x (andexp 'y (notexp 'z))))) ; x=nil z=1 simplifies to nil
-(setq p2 (andexp (andexp 'z 1) (orexp 'x 1))) ; z = 1 x= nil simplfies to 1
-(setq p3 (orexp (notexp 'z)(andexp 'z 1))) ; z = nil simplfies to nil
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(setq p1 (andexp 'x (orexp 'x (andexp 'y (notexp 'z))))) 
+(setq p2 (andexp (andexp 'z 1) (orexp 'x 1)))
+(setq p3 (orexp (notexp 'z)(andexp 'z 1)))
 
 
 (defun evalexp (main-list bind-list)
@@ -27,29 +19,19 @@
 (print main-list)
 (print "The simplified version of your expression is:")
 (simplify main-list)
-
-
-
 	
-);end of evalexp function
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+)
 
 
 (defun loop-binding (main-list bind-list)
-  (compare main-list (car bind-list)) ;first just compare as I can make sure there is a binding list by checking in eval
+  (compare main-list (car bind-list)) 
 
 (if (eq nil (atom (cdr bind-list)) ) (loop-binding main-list (cdr bind-list) )
   
-);end of if
+)
 
 
-);end of loop-binding
-
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
+)
 
 
 (defun compare (main-list binding)
@@ -60,28 +42,26 @@
 
      (progn 
 
-       (if (eq t (atom (car main-list) )) (if (eq (car main-list) (car binding)) (rplaca main-list (car(cdr binding)))) ) ;Here will check the car and if it is not a cons cell, compare it to the bind-list to substitute values
+       (if (eq t (atom (car main-list) )) (if (eq (car main-list) (car binding)) (rplaca main-list (car(cdr binding)))) ) 
 
-       (if (eq nil (atom (car main-list))) (compare (car main-list) binding)   ) ; Here checks if car is a cons, if it is, just recurse that first as to move from left to right of the list 
+       (if (eq nil (atom (car main-list))) (compare (car main-list) binding)   ) 
 
-       (if (eq nil (atom (cdr main-list))) (compare (cdr main-list)binding) ) ;Here begin to check cdrs to see if it points to any cons, if yes, recurse that.
+       (if (eq nil (atom (cdr main-list))) (compare (cdr main-list)binding) )
 
 
-               ) ;else
+               )
 
-     ) ;END OF IF
+     )
   
 
-);end of compare function
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+)
 
 (defun simplify (main-list)
 
   (if  (or  (and  (eq 'NOT(car main-list))  (eq t (atom (car(cdr main-list))) )  (eq nil (cdr(cdr main-list)))  )  
-            (and (or (eq 'and(car main-list)) (eq 'or(car main-list))  )  (eq t (atom (car(cdr main-list))) )  (eq t (atom (car(cdr(cdr main-list)))))                  )) ;IF THIS
+            (and (or (eq 'and(car main-list)) (eq 'or(car main-list))  )  (eq t (atom (car(cdr main-list))) )  (eq t (atom (car(cdr(cdr main-list)))))                  ))
 
-      (progn ;(print "the non-recursive part running")
+      (progn 
 
        (if (eq 'and (car main-list)) ;if it equals and, follow these rules
         (if (or (eq nil (car (cdr main-list))) (eq nil (car(cdr(cdr main-list))))  )   (setq z nil)
@@ -106,14 +86,11 @@
          )
        
 
-      z) ;THEN THIS (HERE CHECK WHETHER ITS AND/OR/NOT AND SIMPLIFY BY FOLLOWING THE RULES
+      z)
 
 
 
-;;THE RECURSIVE PART
-
-       (progn ;this part is the recursive parts where if it finds other expressions within expression it will recurse those to get the value
-         ;(print "The recurive part is running")
+       (progn 
          (if (eq 'not (car main-list))
                  (if (eq 1 (simplify (car(cdr main-list)))) (setq j nil)
                    (if (eq nil (simplify (car(cdr main-list)))) (setq j 1)
@@ -169,24 +146,8 @@
                    )
                )
 
-         j);ELSE THIS
+         j)
 
        )    
                
-);end of simplify function
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-
-
-
-
-
-
-;;;;The following code snippet successfully changes a car of a cons
-;;;;the function is : (rplaca x y) which changes the car of x to y and returns modified) x. x must be a cons
-;;;;but y may be any Lisp object
-;;;
-;;;( rplaca  (cdr(car(cdr(cdr main-list))))  (car(cdr(car bind-list)))  )
-;;;(print main-list)
+)
